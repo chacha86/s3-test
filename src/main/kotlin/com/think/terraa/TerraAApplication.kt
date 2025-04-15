f1a660e766ce927522824d26c307ab2819c16c96
chacha86 <taejincha0619@gmail.com>
2025-04-15 17:07:21 +0900

    work

---
+++ src/main/kotlin/com/think/terraa/TerraAApplication.kt
@@ -0,0 +1,11 @@
+package com.think.terraa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TerraAApplication
+
+fun main(args: Array<String>) {
+    runApplication<TerraAApplication>(*args)
+}
